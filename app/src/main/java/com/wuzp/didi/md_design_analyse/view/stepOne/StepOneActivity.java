577fc7904ecc513c19f111220b337cb7f8fdb9fb
wuzhenpeng03 <wuzhenpengh03@didichuxing.com>
2017-12-15 11:11:43 +0800

    [MDDesignAnalyse]Branch-StepOne feat:添加基本的MD使用方式 description: modify by:wuzhenpeng03

---
+++ app/src/main/java/com/wuzp/didi/md_design_analyse/view/stepOne/StepOneActivity.java
@@ -0,0 +1,20 @@
+package com.wuzp.didi.md_design_analyse.view.stepOne;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wuzp.didi.md_design_analyse.R;
+
+/**
+ * @author wuzhenpeng03 (wuzhenpeng03@didichuxing.com)
+ */
+public class StepOneActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_one_base);
+    }
+}
